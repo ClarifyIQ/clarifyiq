db2ac76929458b676f6c5615942d7c89c11917af
ClarifyIQ <contacto.clarifyiq@gmail.com>
2026-05-26 22:04:18 -0300

    agrega webhook meta

--- index.js
+++ index.js
@@ -57,3 +57,17 @@ app.post('/reset/:telefono', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
+app.get('/webhook', (req, res) => {
+  const VERIFY_TOKEN = process.env.VERIFY_TOKEN || 'clarify2024';
+
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    return res.status(200).send(challenge);
+  }
+
+  return res.sendStatus(403);
+});
